65cbf90ae2886e1d83224908de16e5997f81fb7c
walwal-99 <ks1843@naver.com>
2022-06-29 17:19:43 +0900

    띄어쓰긔

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -9,13 +9,13 @@ public class MainClass {
     public static void main(String[] args) {
         System.out.println("디비를 연결 해보자");
         Scanner sc = new Scanner(System.in);
-        System.out.println("아이템 등록:");
+        System.out.println("아이템 등록: ");
         String name = sc.next();
-        System.out.println("속성 입력:");
+        System.out.println("속성 입력: ");
         String att = sc.next();
-        System.out.println("데미지 입력(숫자만):");
+        System.out.println("데미지 입력(숫자만): ");
         String dam = sc.next();
-        System.out.println("효과 입력:");
+        System.out.println("효과 입력: ");
         String hyo = sc.next();
 
         DBClass dc = new DBClass();
